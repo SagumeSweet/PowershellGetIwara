a8d4628ed80a8abbb5f61a5e54e51866bba21445
Sagume_Sweet <kaguyakochiya@gmail.com>
2023-01-26 23:57:45 +0800

    remove yt-dlp

--- PowershellGetIwara.ps1
+++ PowershellGetIwara.ps1
@@ -1,8 +1,19 @@
-﻿# cookie和UserAgent变量
-$chromeCookies = ''
-$userAgent = ''
+﻿param (
+    [String] $CHROME_COOKIES,
+    [String] $uri,
+    [String] $OUTPUT
+)
+$IWARA_HOME = "https://ecchi.iwara.tv"
+# 变量
+# $CHROME_COOKIES = ''
+$USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+# $OUTPUT = 
+# $uri = 
 
-$uri = "https://ecchi.iwara.tv/subscriptions"
+if ($OUTPUT[-1] -ne "\") {
+    $OUTPUT += "\"
+}
+
 
 function getWebPage {
     param (
@@ -10,48 +21,254 @@ function getWebPage {
         [String] $uri
     )
 
-    $request = Invoke-WebRequest -Uri $uri -SessionVariable "session" -UserAgent $userAgent
+    $request = Invoke-WebRequest -Uri $uri -SessionVariable "session" -UserAgent $USER_AGENT
     $chomeCookiesList = $chromeCookies -split "; "
     foreach ($chormeCookie in $chomeCookiesList) {
         $cookieList = $chormeCookie -split "="
         $cookieClass = [System.Net.Cookie]::new($cookieList[0], $cookieList[1])
         $session.Cookies.Add($uri, $cookieClass)
     }
-    $request = Invoke-WebRequest -Uri $uri -WebSession $session -UserAgent $userAgent
+    $request = Invoke-WebRequest -Uri $uri -WebSession $session -UserAgent $USER_AGENT
     
     return $request
 }
 
-function downloadVideo {
+function downloadAllPageVideos {
     Param (
-        [Microsoft.PowerShell.Commands.HtmlWebResponseObject] $request
+        [String] $chromeCookies,
+        [String] $type,
+        [System.DateTime] $lastTime,
+        [String] $output,
+        $request
     )
 
-    [System.Collections.ArrayList]$videoArray = 'a', 'b'
+    $videoArray = @()
     foreach ($link in $request.Links.href) {
         if (($link -like '/videos/*') -and ($link -notin $videoArray)) {
-            $videoURL = "https://ecchi.iwara.tv" + $link
-            $videoArray.add($link)
-            Write-Host $videoURL
+            $videoArray += $link
+            Write-Host $link
+            $downloadError = downloadVideo -uri $link -chromeCookies $chromeCookies -type $type -lastDate $lastTime -output $output
+            if ($downloadError -eq 3) {
+                return 1
+            }
             # yt-dlp命令
-            & '.\yt-dlp.exe' --config-location '.\local.conf' -P '.' -f "Source" --dateafter 20221025 $videoURL
+            # & '.\yt-dlp.exe' --config-location '.\local.conf' -P '.' -f "Source" --dateafter 20221025 $videoURL
         }
+    }
+    return "INFO:Download all page videos function finished"
+}
+
+function getUriParts {
+    Param (
+        [String] $uri
+    )
+    $parts = $uri.split('/')
+    return $parts
+}
+
+function infoRegex {
+    param (
+        [String] $inputStr,
+        [String] $regexStr
+    )
+    foreach ($match in [System.Text.RegularExpressions.Regex]::Matches($inputStr,$regexStr)) {
+        return $match.Value
     }
 }
 
-
-$request = getWebPage -chromeCookies $chromeCookies -uri $uri
-$page = 1
-while ($request.Content -notmatch '<li class="pager-next last">&nbsp;</li>') {
-    Write-Host '===================================================================================================================='
-    Write-Host ' '
-    Write-Host $nextURL
-    Write-Host ("Page = " + $page.ToString())
-    $nextURL = ("https://ecchi.iwara.tv/subscriptions?page=" + $page.ToString())
-    $page += 1
-    downloadVideo -request $request
-    $request = getWebPage -chromeCookies $chromeCookies -uri $nextURL
+function getDateFromString {
+    Param (
+        [String] $dateStr
+    )
+    $date = Get-Date -Date $dateStr
+    return $date
 }
 
-Write-Host "end:"
-downloadVideo -request $request
+function getLastDate {
+    $dateStr = Get-Content -Path .\LastDate.txt
+    $date = getDateFromString -dateStr $dateStr
+    return $date
+}
+
+function getVideoinfo {
+    Param (
+        [String] $chromeCookies,
+        [String] $uri
+    )
+
+    if ($uri[0] -ne "h") {
+        $uri = $IWARA_HOME + $uri
+    }
+    $request = getWebPage -uri $uri -chromeCookies $chromeCookies
+    $content = $request.Content
+    if (!($content -notmatch "<h1>Private video</h1>")) {
+        return 0
+    }
+    $allInfo = infoRegex -inputStr $content -regexStr '<h1 class="title">(.|\n)*?作成者:.+'
+    $userName = infoRegex -inputStr $allInfo -regexStr '(?<=class="username">).*(?=<\/a>)'
+    $title = infoRegex -inputStr $allInfo -regexStr '(?<=<h1 class="title">).*(?=<\/h1>)'
+    $dateStr = infoRegex -inputStr $allInfo -regexStr '(?<=作成日:).*(?= )'
+    $date = getDateFromString -dateStr $dateStr
+    return @{"title" = $title; "userName" = $userName; "date" = $date}
+}
+
+function stringToFileName {
+    param (
+        [String] $str
+    )
+    $result = $str -replace "/", ""
+    $result = $result -replace "\\", ""
+    $result = $result -replace ":", ""
+    $result = $result -replace "\*", ""
+    $result = $result -replace '"', ""
+    $result = $result -replace "\?", ""
+    $result = $result -replace "<", ""
+    $result = $result -replace ">", ""
+    $result = $result -replace "|", ""
+    return $result
+}
+
+function downloadVideo {
+    Param (
+        [String] $uri,
+        [String] $chromeCookies,
+        [System.DateTime] $lastDate,
+        [String] $type,
+        [String] $output
+    )
+
+    $uriParts = getUriParts -uri $uri
+    $videoUri = "https://ecchi.iwara.tv/videos/"
+    if ($uriParts -contains "videos") {
+        $videoUri += $uriParts[-1]
+    } else {
+        Write-Host "ERROR:Not a video"
+        return 1
+    }
+    Write-Host $videoUri
+    $info = getVideoinfo -uri $uri -chromeCookies $chromeCookies
+    if ($info -eq 0) {
+        Write-Host "ERROR:Private video"
+        return 2
+    } elseif (([System.DateTime]::Compare($info["date"], $lastDate) -lt 0) -and ($type -eq "Subscriptions")) {
+        Write-Host "ERROR:Date is earlier than last time"
+        return 3
+    }
+    $apiURI = "https://ecchi.iwara.tv/api/video/" + $uriParts[-1]
+    $request = Invoke-WebRequest -Uri $apiURI -UserAgent $USER_AGENT
+    $jsonArray = (ConvertFrom-Json -InputObject $request.Content)
+    $downloadURL = "https:"
+    $ext = "."
+    foreach ($json in $jsonArray) {
+        if ($json.resolution -eq "Source") {
+            $downloadURL += $json.uri
+            $ext += (infoRegex -inputStr $json.mime -regexStr "(?<=.*\/).*")
+            break
+        }
+    }
+    # 输出到作者名文件夹
+    $folderName = stringToFileName -str $info["userName"]
+    $fileName = stringToFileName -str $info["title"]
+    $outputFile = $output + $folderName + "\" + $fileName + $ext
+    $outputFolder = $output + $folderName + "\"
+    if (!(Test-Path -Path $outputFolder)) {
+        Write-Host "ERROE:Path not exitst"
+        New-Item -Path $output -Name $folderName -ItemType Directory
+        Write-Host "INFO:Make dir end"
+    }
+    if (Test-Path -Path $outputFile) {
+        Write-Host "INFO:Folder Path Succeesfully"
+        Write-Host "ERROR:Video has been downloaded"
+        return 4
+    }
+    Write-Host "INFO:Download file:" $outputFile
+    Start-BitsTransfer -Source $downloadURL -Destination $outputFile
+    return "INFO:Download video function finished"
+}
+
+function downloadALlPage {
+    param (
+        [String] $chromeCookies,
+        [System.DateTime] $lastTime,
+        [String] $uri,
+        [String] $type,
+        [String] $output
+    )
+    
+    $request = getWebPage -uri $uri -chromeCookies $chromeCookies
+    $page = 1
+    while (!($request.Content -notmatch '<a title="次のページへ"')) {
+        Write-Host '===================================================================================================================='
+        Write-Host ' '
+        Write-Host $nextURL
+        Write-Host ("Page = " + $page.ToString())
+        # 获取下一页
+        $nextURL = $IWARA_HOME + (infoRegex -inputStr $request.content -regexStr '(?<=<a title="次のページへ" href=").*(?=">)')
+        $page += 1
+        $downloadPageError = downloadAllPageVideos -request $request -type $type -lastTime $lastTime -chromeCookies $chromeCookies -output $output
+        if (($downloadPageError -eq 1) -and ($type -eq "Subscriptions")) {
+            return 1
+        }
+        $request = getWebPage -uri $nextURL -chromeCookies $chromeCookies
+    }
+    Write-Host "end:"
+    downloadAllPageVideos -request $request -type $type -lastTime $lastTime -chromeCookies $chromeCookies -output $output
+    return "INFO:Download all pages function finished"
+
+}
+
+function downloadSubscriptions {
+    Param (
+        [String] $chromeCookies,
+        [System.DateTime] $lastTime,
+        [String] $uri,
+        [String] $output
+    )
+    $type = "Subscriptions"
+    $downloadError = downloadALlPage -chromeCookies $chromeCookies -lastTime $lastTime -uri $uri -output $output -type $type
+    if ($downloadError -eq 0) {
+        $newDate = Get-Date
+        $newDate = $newDate.AddDays(-1)
+        Out-File -FilePath .\LastDate.txt -InputObject $newDate.ToString("yyyy-MM-dd")
+    }
+    return "INFO:Download subscriptions function finished"
+}
+
+function downloadFromUsers {
+    param (
+        [String] $chromeCookies,
+        [System.DateTime] $lastTime,
+        [String] $uri,
+        [String] $output
+    )
+    
+    $type = "Users"
+    $userPage = getWebPage -uri $uri -chromeCookies $chromeCookies
+    if ($userPage.Content -notmatch "See all  </a>") {
+        downloadAllPageVideos -chromeCookies $chromeCookies -type $type -lastTime $lastTime -output $output -request $userPage
+    } else {
+        $uri = $IWARA_HOME + (infoRegex -inputStr $userPage.Content -regexStr '(?<=<div class="more-link">[\s]*<a href=").*(?=(">[\s]*See all))')
+        downloadALlPage -chromeCookies -$chromeCookies -type $type -lastTime $lastTime -uri $uri -output $output
+    }
+    return "INFO:Download from users function finished"
+}
+
+if (!(Test-Path -Path .\LastDate.txt)) {
+    Set-Content -Path .\LastDate.txt -Value "2022-12-01"
+}
+$LAST_DATE = getLastDate
+
+switch ($uri.split("/")[3]) {
+    "subscriptions" {
+        downloadSubscriptions -uri $uri -chromeCookies $CHROME_COOKIES -lastTime $LAST_DATE -output $OUTPUT
+    }
+    "videos" {
+        downloadVideo -uri $uri -chromeCookies $CHROME_COOKIES -lastDate $LAST_DATE -type "Video" -output $OUTPUT
+    }
+    "users" {
+        downloadFromUsers -uri $uri -chromeCookies $CHROME_COOKIES -lastTime $LAST_DATE -output $OUTPUT
+    }
+    Default {
+        Write-Host "ERROR:Unsupported URL"
+    }
+}
--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 ## 使用方法  
 
-+ 先把yt-dlp装上，配置好yt-dlp设置和脚本内的yt-dlp命令
-+ `powershell *.ps1 -chromeCookies cookie -userAgent UserAgent`  
-直接在脚本里设置变量也可
++ 本分支使用`Start-BitsTransfer`下载，也可以换成`Invoke-Webrequest`,不用额外装下载器
++ 支持从单个视频，用户，订阅里下载，不支持观看列表下载
++ `powershell *.ps1 -CHROME_COOKIES cookie -uri URL -OUTPUT outputDic`  
+直接在脚本里设置变量也可,useragent直接在脚本里设置，反正也不改
 + cookie和useragent从浏览器直接复制即可  
-
-## 如何实现的
-
-+ 通过cookie登录，yt-dlp批量下载关注更新的视频，筛选通过yt-dlp配置filter  
-+ 访问subscriptions页面获取视频链接然后yt-dlp下载，直到没有下一页。/subscriptions?page=* 增加页数， 视频的链接都是在 /videos/\* 下  
++ 下载到作者名字文件夹下 `output\username\video`
++ 最好别和yt-dlp混用，因为下载的文件名不一样，到时候一堆重复的
++ 下载订阅有个功能还没测试，理论上是每次下载后记录下时间，比这个早的直接跳过
